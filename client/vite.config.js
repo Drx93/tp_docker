d1ce133496c8a8e0e94456a292f8e5ed1a088d54
Drx93 <dorian93.roux@gmail.com>
2026-01-07 15:31:31 +0100

    fix bug

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,10 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     port: 5173,
     proxy: {
-      '/api': 'http://localhost:3000',
-      '/auth': 'http://localhost:3000'
+      '/api': 'http://backend:3000',
+      '/auth': 'http://backend:3000'
     }
   }
 });
