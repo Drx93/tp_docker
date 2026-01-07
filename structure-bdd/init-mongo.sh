d7ce3cc95bd97fef8e0367e8ecf825e081143e17
Drx93 <dorian93.roux@gmail.com>
2026-01-07 15:57:58 +0100

    fix

--- structure-bdd/init-mongo.sh
+++ structure-bdd/init-mongo.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# Script d'initialisation MongoDB
-# Importe les restaurants dans la base de donnees
-
-echo "Import des restaurants dans MongoDB..."
+echo 'Import des restaurants dans MongoDB...'
 mongoimport --db orium_db --collection restaurants --file /docker-entrypoint-initdb.d/restaurants.json --jsonArray
-echo "Import termine!"
+echo 'Import termine!'
